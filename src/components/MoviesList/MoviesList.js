7ac0731097298eb0dfd9ea0166ca2217ce0ee8f6
Alexandr Lisovoy <sashaliss18@gmail.com>
2024-03-13 18:42:12 +0500

    addMovieOnPage

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -3,39 +3,42 @@ import './MoviesList.css';
 import { Flex } from 'antd';
 
 import MoviesListItem from '../MoviesListItem/MoviesListItem';
-// import MoviesService from '../../services/MoviesService';
+import MoviesService from '../../services/MoviesService';
 
 export default class MoviesList extends Component {
-  // moviesServices = new MoviesService();
+  moviesServices = new MoviesService();
 
-  // constructor() {
-  //   super();
+  maxId = 1;
 
-  //   this.state = {
-  //     movies: [],
-  //   };
+  constructor() {
+    super();
 
-  //   this.getMovies();
-  // }
+    this.state = {
+      movies: [],
+    };
 
-  // getMovies = () => {
-  //   this.moviesServices.getMoviesData().then(res => {
-  //     this.setState({ movies: res });
-  //   });
-  // };
+    this.getMovies();
+  }
+
+  getMovies = () => {
+    this.moviesServices.getMoviesData().then(res => {
+      this.setState({ movies: res });
+    });
+  };
 
   render() {
-    // const { movies } = this.state;
-    // eslint-disable-next-line no-console
-    // console.log(movies);
+    const { movies } = this.state;
 
     return (
       <Flex className="reset-list" horizontal="true" wrap="wrap" gap="middle">
-        <MoviesListItem />
-        <MoviesListItem />
-        <MoviesListItem />
-        <MoviesListItem />
-        <MoviesListItem />
+        {movies.map(movie => (
+          <MoviesListItem
+            key={this.maxId++}
+            title={movie.title}
+            date={movie.release_date}
+            description={movie.overview}
+          />
+        ))}
       </Flex>
     );
   }
--- src/components/MoviesListItem/MoviesListItem.js
+++ src/components/MoviesListItem/MoviesListItem.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 
 import { Flex, Card } from 'antd';
 import './MoviesListItem.css';
+import PropTypes from 'prop-types';
 
 import MovieGenres from '../MovieGenres/MovieGenres';
 
@@ -9,15 +10,29 @@ const { Meta } = Card; // antd
 
 export default class MoviesListItem extends Component {
   render() {
+    const { title, date, description } = this.props;
+
     return (
       <Flex className="movie" horizontal="true">
         <div className="image">Image</div>
         <Card className="info" bordered={false}>
-          <Meta title="Title" description="Data" />
+          <Meta title={title} description={date} />
           <MovieGenres />
-          <p>Description</p>
+          <p>{description}</p>
         </Card>
       </Flex>
     );
   }
 }
+
+MoviesListItem.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  description: PropTypes.string,
+};
+
+MoviesListItem.defaultProps = {
+  title: 'Unknown',
+  date: 'Unknown',
+  description: 'None',
+};
