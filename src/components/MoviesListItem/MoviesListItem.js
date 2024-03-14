b223f4de5112964c97adf2a498adde61bfeec034
Alexandr Lisovoy <sashaliss18@gmail.com>
2024-03-14 14:04:55 +0500

    limitTextAndReleaseDateAdd

--- src/components/MoviesListItem/MoviesListItem.js
+++ src/components/MoviesListItem/MoviesListItem.js
@@ -3,12 +3,29 @@ import { Component } from 'react';
 import { Flex, Card } from 'antd';
 import './MoviesListItem.css';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 
 import MovieGenres from '../MovieGenres/MovieGenres';
 
 const { Meta } = Card; // antd
 
 export default class MoviesListItem extends Component {
+  limitDescription(text) {
+    if (!text) return '// Author did not provide a description //';
+
+    this.str = text.slice(0, 180);
+
+    if (this.str.length < text.length) {
+      this.arr = this.str.split(' ');
+      this.arr.splice(this.arr.length - 1, 1);
+      this.str = this.arr.join(' ');
+
+      return `${this.str} ...`;
+    }
+
+    return text;
+  }
+
   render() {
     const { title, date, description } = this.props;
 
@@ -16,9 +33,14 @@ export default class MoviesListItem extends Component {
       <Flex className="movie" horizontal="true">
         <div className="image">Image</div>
         <Card className="info" bordered={false}>
-          <Meta title={title} description={date} />
+          <Meta
+            title={title}
+            description={
+              date ? format(date, 'MMMM d, y') : 'Release date unknown'
+            }
+          />
           <MovieGenres />
-          <p>{description}</p>
+          <p>{this.limitDescription(description)}</p>
         </Card>
       </Flex>
     );
