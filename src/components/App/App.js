e9099cac86c004df294b30063a6ae9432aba3a1d
Alexandr Lisovoy <sashaliss18@gmail.com>
2024-03-15 17:56:19 +0500

    offlineErrorsLoadingComplete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,67 @@
 import './App.css';
 
+import { Component } from 'react';
+
+import { Offline } from 'react-detect-offline';
+import { Alert } from 'antd';
+import Marquee from 'react-fast-marquee';
+
 import MoviesList from '../MoviesList/MoviesList';
+import MoviesService from '../../services/MoviesService';
 
-function App() {
-  return (
-    <div className="container">
-      <MoviesList />
-    </div>
-  );
+export default class App extends Component {
+  moviesServices = new MoviesService();
+
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      loading: true,
+      error: false,
+    };
+
+    this.getMovies();
+  }
+
+  onLoadingMovies = res => {
+    this.setState({ movies: res, loading: false });
+  };
+
+  onErrorMovies = () => {
+    this.setState({ error: true, loading: false });
+  };
+
+  getMovies = () => {
+    this.moviesServices
+      .getMoviesData()
+      .then(this.onLoadingMovies)
+      .catch(this.onErrorMovies);
+  };
+
+  render() {
+    const { movies, loading, error } = this.state;
+
+    const offlineWarning = (
+      <Alert
+        style={{ position: 'fixed', width: '100%', zIndex: '10' }}
+        banner
+        message={
+          <Marquee pauseOnHover>
+            No internet connection // It might be worth checking your connection
+            VPN
+          </Marquee>
+        }
+      />
+    );
+
+    return (
+      <>
+        <Offline>{offlineWarning}</Offline>
+        <div className="container">
+          <MoviesList movies={movies} loading={loading} error={error} />
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,49 +1,68 @@
-import { Component } from 'react';
-
 import './MoviesList.css';
 
-import { Flex } from 'antd';
+import { Component } from 'react';
+
+import PropTypes from 'prop-types';
+import { Flex, Spin, Alert } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 import MoviesListItem from '../MoviesListItem/MoviesListItem';
-import MoviesService from '../../services/MoviesService';
 
 export default class MoviesList extends Component {
-  moviesServices = new MoviesService();
-
   maxId = 1;
 
-  constructor() {
-    super();
-
-    this.state = {
-      movies: [],
-    };
-
-    this.getMovies();
-  }
-
-  getMovies = () => {
-    this.moviesServices.getMoviesData().then(res => {
-      this.setState({ movies: res });
-    });
-  };
-
   render() {
-    const { movies } = this.state;
+    const { movies, loading, error } = this.props;
+
+    const hasData = !(loading || error);
+
+    const spinner = loading ? (
+      <Spin indicator={<LoadingOutlined style={{ fontSize: 44 }} spin />} />
+    ) : null;
+
+    const content = hasData
+      ? movies.map(movie => (
+          <MoviesListItem
+            key={this.maxId++}
+            title={movie.title}
+            date={movie.release_date}
+            description={movie.overview}
+          />
+        ))
+      : null;
+
+    const errorMessage = error ? (
+      <Alert
+        style={{ whiteSpace: 'pre-line' }}
+        message="Error"
+        description={`Access to the list of films is temporarily unavailable :(
+          Try turning it on VPN or reload the page. 
+          If this doesn’t help, then we are already working on the problem!`}
+        type="error"
+        showIcon
+      />
+    ) : null;
 
     return (
       <ul className="reset-list">
-        <Flex horizontal="true" wrap="wrap" gap="large">
-          {movies.map(movie => (
-            <MoviesListItem
-              key={this.maxId++}
-              title={movie.title}
-              date={movie.release_date}
-              description={movie.overview}
-            />
-          ))}
+        <Flex horizontal="true" wrap="wrap" gap="large" justify="center">
+          {spinner}
+          {content}
+          {errorMessage}
         </Flex>
       </ul>
     );
   }
 }
+
+MoviesList.propTypes = {
+  movies: PropTypes.array,
+  loading: PropTypes.bool,
+  error: PropTypes.bool,
+};
+
+MoviesList.defaultProps = {
+  movies: [],
+  loading: false,
+  error: false,
+};
